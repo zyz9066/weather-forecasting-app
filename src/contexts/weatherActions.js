a6c0bf704ed3c5f22b9f051d9b00be4c90dadebf
Tianye Zhao <zyz9066@yahoo.com>
2025-12-07 15:46:24 -0500

    Update README

--- src/contexts/weatherActions.js
+++ src/contexts/weatherActions.js
@@ -1,6 +1,6 @@
 import { getWeatherByCity, getForecastByCity, getWeatherByCoords, getForecastByCoords } from "../api/weatherApi";
 
-const API_KEY = import.meta.env.VITE_WEATHER_API_KEY || process.env.REACT_APP_WEATHER_API_KEY;
+const API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
 
 const actionCreators =  {
   // Simple state setters
